9bc8c1564d50b3f6c1d82c6dca6aa805917ee6c2
zzz999tokdalang999zzz-svg <your-email@example.com>
2025-08-27 20:20:41 +0700

    Add config.php file and update .gitignore

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('DB_HOST')) {
+    define('DB_HOST', '	sql301.byethost24.com');
+    define('DB_USER', '	b24_39797527');
+    define('DB_PASS', ' thanhlongpfl');
+    define('DB_NAME', 'b24_39797527_portfolio_project');
+}
+
+// Cloudinary Configuration
+if (!defined('CLOUDINARY_CLOUD_NAME')) {
+    define('CLOUDINARY_CLOUD_NAME', 'dfv2ibaba'); // Thay thế bằng cloud name của bạn
+    define('CLOUDINARY_API_KEY', '591638797321952'); // Thay thế bằng API key của bạn
+    define('CLOUDINARY_API_SECRET', 'qxXlBKp_I4ckef2_RNsnNwT71nI'); // Thay thế bằng API secret của bạn
+}
